5dbe6b1cdba4d2fb70d643c7ad72fba24434939d
mdouchement <marc.douchement@predicsis.com>
2015-04-26 18:53:16 +0200

    Add track edit form

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,7 +3,23 @@ class TracksController < ApplicationController
     @tracks = Track.all
   end
 
-  def show
+  def edit
     @track = Track.find(params[:id])
+  end
+
+  def update
+    track = Track.find(params[:id])
+    if track.update_attributes(track_params)
+      redirect_to tracks_path
+    else
+      flash[:alert] = track.errors.full_messages
+      redirect_to edit_track_path(track.id)
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:track_number, :artist, :album, :title)
   end
 end
